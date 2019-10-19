d3ca5e15c082241ccc4b348c56b543ed021caa97
SOOHYUNLIM <sh_7682@naver.com>
2019-10-19 17:22:14 +0900

    dd

---
+++ zerock/src/main/test/HelloTest.java
@@ -0,0 +1,17 @@
+package org.zerock;
+
+import org.junit.Test;
+
+/**
+ * HelloTest
+ */
+public class HelloTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+    
+}
